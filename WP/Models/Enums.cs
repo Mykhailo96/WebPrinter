95c6a971e4a92787e22bafe29ebffad24292a496
Mykhailo Voronovskii <Mykhailoblack@ukr.net>
2016-05-07 18:21:45 +0300

    Enums for purchase

---
+++ WP/Models/Enums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WP.Models
+{
+    public enum Precision
+    {
+        Mk100,
+        Mk200,
+        Mk300
+    }
+
+    public enum Color
+    {
+        White,
+        Black,
+        Yellow,
+        Red,
+        Green,
+        Blue
+    }
+
+    public enum Material
+    {
+        ABS,
+        PLA
+    }
+}
