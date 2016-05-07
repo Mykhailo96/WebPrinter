fb1da1e08d2b3022c5a3f2b750458030390a0a15
Mykhailo Voronovskii <Mykhailoblack@ukr.net>
2016-05-07 16:44:24 +0300

    Add project files.

---
+++ WP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WP.Startup))]
+namespace WP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
