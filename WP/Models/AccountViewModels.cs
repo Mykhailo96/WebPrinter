db2629f7d29de7eb6888fed71fdd3cd4f690b459
Mykhailo Voronovskii <Mykhailoblack@ukr.net>
2016-05-21 00:26:18 +0300

    Password reser functionality

--- WP/Models/AccountViewModels.cs
+++ WP/Models/AccountViewModels.cs
@@ -102,7 +102,7 @@ namespace WP.Models
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "New password")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
