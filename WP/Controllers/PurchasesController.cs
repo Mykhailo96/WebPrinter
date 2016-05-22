fd386b560449465682125b0b37e50a1c3b41d2ef
Mykhailo Voronovskii <Mykhailoblack@ukr.net>
2016-05-22 22:36:08 +0300

    User can't edit order info after status changing

--- WP/Controllers/PurchasesController.cs
+++ WP/Controllers/PurchasesController.cs
@@ -83,6 +83,10 @@ namespace WP.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Purchase purchase = db.Purchases.Find(id);
+            if(purchase.OrderStatus.ToString() != "Pending")
+            {
+                return RedirectToAction("Details", new { id = id});
+            }
             if (purchase == null)
             {
                 return HttpNotFound();
