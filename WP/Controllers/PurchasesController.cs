475e301bc1a5516de20aa12b4241ecaa1b0c8591
Mykhailo Voronovskii <Mykhailoblack@ukr.net>
2016-05-22 22:45:10 +0300

    Fixed bug

--- WP/Controllers/PurchasesController.cs
+++ WP/Controllers/PurchasesController.cs
@@ -83,7 +83,7 @@ namespace WP.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Purchase purchase = db.Purchases.Find(id);
-            if(purchase.OrderStatus.ToString() != "Pending")
+            if(purchase.OrderStatus.ToString() != "Pending" && User.Identity.Name != "Admin")
             {
                 return RedirectToAction("Details", new { id = id});
             }
